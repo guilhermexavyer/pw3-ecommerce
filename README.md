16984a590d107003a1dbd960e98615f5d8605c7f
guilhermexavyer <guilhermexavierpessoal@gmail.com>
2024-10-18 19:40:43 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Aprendendo a programar em Dart
+# ecommerce
 
-Este é um projeto que foi (está sendo) desenvolvido para a disciplina **Programação para web III** do curso **Tecnologia em Análise e Desenvolvimento de Sistemas**, ministrada pelo professor **Murilo Silva**, docente do **Instituto Federal de Goiás, Câmpus Jataí, Unidade Flamboyant**.
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista de Tarefas',
+      home: TodoList(),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _items = [];
+
+  void _addItem() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _items.add(_controller.text);
+        _controller.clear();
+      });
+    }
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      _items.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Tarefas'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: 'Digite algo'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _addItem,
+              child: Text('Adicionar'),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_items[index]),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () => _removeItem(index),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
