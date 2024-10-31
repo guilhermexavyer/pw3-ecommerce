f0a9238301bfde9d86fbedfb463a473a5a9ae514
guilhermexavyer <guilhermexavierpessoal@gmail.com>
2024-10-31 18:17:04 -0300

    Atividade Avaliativa 24-10-2024

--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -1,45 +1,21 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import '../models/product.dart';
 
 class ProductRepository {
-  final String baseUrl =
-      "http://localhost:8000"; // Troque pela URL do seu backend
+  final List<Product> _products = [];
 
+  // Busca todos os produtos
   Future<List<Product>> fetchProducts() async {
-    final response = await http.get(Uri.parse('$baseUrl/products'));
-
-    if (response.statusCode == 200) {
-      List<dynamic> body = jsonDecode(response.body);
-      List<Product> products =
-          body.map((item) => Product.fromJson(item)).toList();
-      return products;
-    } else {
-      throw Exception('Failed to load products');
-    }
+    return List.from(_products); // Retorna uma cópia da lista de produtos
   }
 
+  // Cria e adiciona um novo produto à lista
   Future<Product> createProduct(Product product) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/product/save'),
-      headers: {"Content-Type": "application/json"},
-      body: jsonEncode(product.toJson()),
-    );
-
-    if (response.statusCode == 200) {
-      return Product.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to create product');
-    }
+    _products.add(product);
+    return product; // Retorna o produto criado
   }
 
+  // Remove um produto da lista pelo ID
   Future<void> deleteProduct(int id) async {
-    final response = await http.delete(Uri.parse('$baseUrl/product/$id'));
-
-    /// Throws an [Exception] if the request was not successful.
-
-    if (response.statusCode != 200) {
-      throw Exception('Failed to delete product');
-    }
+    _products.removeWhere((product) => product.id == id);
   }
 }
